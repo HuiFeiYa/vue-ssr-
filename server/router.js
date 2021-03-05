f820966c97dfc66c993216b845f04c3767b38203
宋远桥 <wanxiao.guan@ideacome.com>
2021-03-05 13:57:24 +0800

    [feature] 设置返回文件名

--- server/router.js
+++ server/router.js
@@ -2,8 +2,16 @@ const Router = require('koa-router')
 const router = new Router()
 const isDev = process.env.NODE_ENV === 'development'
 const { log } = require('./controller/index')
+const fs = require('fs')
 const handleRequest = isDev ? require('./dev') : require('./prd')
 // 和 src/router 中的routes 保持一致，定义路由是为了使那些没有注册的路由会走到 notFound 中间件的逻辑中
 router.get('/index', log, handleRequest)
-
+router.get('/xmind', async ctx=>{
+  const image = fs.readFileSync('./public/vue.png')
+  // ctx.set({
+  //   'Content-Type':'application/x-msdos-program'
+  // })
+  ctx.attachment('xmind.exe')
+  ctx.body = image
+})
 module.exports = router
